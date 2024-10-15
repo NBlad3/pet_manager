47628eee57e61b65c5d9f9dbb981d3adefc7fc4a
NBlad3 <NBFriend123@outlook.com>
2024-10-15 16:28:29 +0200

    Back up

--- app/models.py
+++ app/models.py
@@ -2,13 +2,13 @@ from app import db
 
 class Dog(db.Model):
     __tablename__ = "dogs"
-    id = db.Column(db.Integer, primary_key = True, auto_increment = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(255), nullable = True)
     age = db.Column(db.Integer, nullable = True)
 
 class Cat(db.Model):
     __tablename__ = "cats"
-    id = db.Column(db.Integer, primary_key = True, auto_increment = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(255), nullable = True)
     age = db.Column(db.Integer, nullable = True)
 
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
+from app.routes import *
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
 
