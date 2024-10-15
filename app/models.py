213eeef4998152c7a52c88428183b74ca856242b
NBlad3 <NBFriend123@outlook.com>
2024-10-15 18:42:01 +0200

    Back up

--- app/models.py
+++ app/models.py
@@ -3,12 +3,12 @@ from app import db
 class Dog(db.Model):
     __tablename__ = "dogs"
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    name = db.Column(db.String(255), nullable = True)
-    age = db.Column(db.Integer, nullable = True)
+    name = db.Column(db.String(255), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
 
 class Cat(db.Model):
     __tablename__ = "cats"
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    name = db.Column(db.String(255), nullable = True)
-    age = db.Column(db.Integer, nullable = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
 
